6f6bdb97467638befb31afe7fd42555685e7cc69
Alvee93 <31181902+Alvee93@users.noreply.github.com>
2018-12-20 16:35:04 +0600

    Refactored code

--- Qt75ExifAuto01/mainwindow.cpp
+++ Qt75ExifAuto01/mainwindow.cpp
@@ -7,12 +7,6 @@
 #include <stdio.h>
 #include "exif.h"
 #include <fstream>
-#include <iostream>
-
-#include <string>
-#include <fstream>
-
-using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -37,48 +31,63 @@ void MainWindow::on_pushButtonBrowse_clicked()
     QDir directory(dir);
     QStringList images = directory.entryList(QStringList() << "*.jpg" << "*.JPG",QDir::Files);
 
-    ofstream myfile;
-    myfile.open ("makemodel.csv");
-        foreach(QString filename, images) {
+
+    int image_count = 0;
+    foreach(QString filename, images) {
         //do whatever you need to do
-            qDebug() << filename;
-            QString full_filename = dir + "/" + filename;
-            QByteArray ba = full_filename.toLatin1();                //Exif Parsing Begins
-            const char *temp = ba.data();
+        qDebug() << filename;
+        QString full_filename = dir + "/" + filename;
+        QByteArray ba = full_filename.toLatin1();                //Exif Parsing Begins
+        const char *temp = ba.data();
 
-            FILE *fp = fopen(temp,"rb");// binPix;
+        FILE *fp = fopen(temp,"rb");// binPix;
 
-            fseek(fp, 0, SEEK_END);
-            unsigned long fsize = ftell(fp);
-            rewind(fp);
-            unsigned char *buf = new unsigned char[fsize];
+        fseek(fp, 0, SEEK_END);
+        unsigned long fsize = ftell(fp);
+        rewind(fp);
+        unsigned char *buf = new unsigned char[fsize];
 
-            if (fread(buf, 1, fsize, fp) != fsize) {
-                //ui->exifError->setText("Cannot read file.");
-                delete[] buf;
-                //return 0;
-              }
-
-            fclose(fp);
-
-            easyexif::EXIFInfo result;
-            int code = result.parseFrom(buf, fsize);
+        if (fread(buf, 1, fsize, fp) != fsize) {
+            //ui->exifError->setText("Cannot read file.");
             delete[] buf;
-            if (code) {
-                //ui->exifError->setText("Error parsing Exif data.");
-              }
+            //return 0;
+          }
 
-            myfile << result.Make.c_str() << "," << result.Model.c_str() << endl;
-            qDebug() << QString::fromStdString(result.Make.c_str())
-                     << ", "
-                     << QString::fromStdString(result.Model.c_str());
+        fclose(fp);
 
+        easyexif::EXIFInfo result;
+        int code = result.parseFrom(buf, fsize);
+        delete[] buf;
+        if (code) {
+            //ui->exifError->setText("Error parsing Exif data.");
+          }
+
+        // FILE OPERATION
+        writeToFile(image_count, result.Make.c_str(), result.Model.c_str());
+        //myfile << result.Make.c_str() << "," << result.Model.c_str() << endl;
+        qDebug() << QString::fromStdString(result.Make.c_str())
+                 << ", "
+                 << QString::fromStdString(result.Model.c_str());
+        image_count++;
         }
 
-    myfile.close();
+    //myfile.close();
+
+    ui->label_image_count->setText(QString::number(image_count));
 }
 
-// new_function_to_write (int i, string make, string model){
-    //if (i==0) create new file and do actions
-    // else {} // edit
-//}
+void MainWindow::writeToFile(int i, string c_make, string c_model)
+{
+    if(i==0){
+        ofstream myfile;
+        myfile.open ("makemodel.csv");
+        myfile << c_make << "," << c_model << endl;
+        myfile.close();
+    }
+    else {
+        fstream myfile;
+        myfile.open ("makemodel.csv", ios::app);
+        myfile << c_make << "," << c_model << endl;
+        myfile.close();
+    }
+}
